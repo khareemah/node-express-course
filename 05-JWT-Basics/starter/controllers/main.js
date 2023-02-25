aa826f267453d2bcaa9ae983fc068ae3acae6ddc
kareemah <ajimobikareemah@gmail.com>
2023-02-25 13:04:06 +0700

    end of project

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -4,8 +4,8 @@
 
 // set up authentication so that only the request with JWT can access the dashboard
 
-const customAPIError = require('../errors/custom-error');
 const jwt = require('jsonwebtoken');
+const { BadRequest } = require('../errors');
 const login = async (req, res) => {
   const { username, password } = req.body;
   // to validate username and pasword, options are:
@@ -13,10 +13,10 @@ const login = async (req, res) => {
   // Joi (extra package)
   // checking in the controller
   if (!username || !password) {
-    throw new customAPIError('please provide username and passwword', 400);
+    throw new BadRequest('Please provide email and password');
   }
 
-  // use for demo, normalluy provided by DB
+  // use for demo, normally provided by DB
   const id = new Date().getTime();
   // try to keep payload small, better experience for user
   const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
@@ -26,24 +26,11 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new customAPIError('no token provided', 401);
-  }
-  const token = authHeader.split(' ')[1];
-  console.log(token);
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `Hello ${decoded.username}`,
-      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-    });
-  } catch (error) {
-    throw new customAPIError('not authorized to access this route');
-  }
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = { login, dashboard };
---
+++ 05-JWT-Basics/starter/errors/index.js
@@ -0,0 +1,4 @@
+const BadRequest = require('./bad-request');
+const Unauthenticated = require('./unauthenticated');
+const CustomAPIError = require('./custom-error');
+module.exports = { BadRequest, Unauthenticated, CustomAPIError };
