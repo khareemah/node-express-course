2a35f1a2aec9dbc5ec8b8db466f384d9ac34a98e
kareemah <ajimobikareemah@gmail.com>
2023-03-10 18:10:03 +0700

    image upload to local server

--- 07-file-upload/starter/app.js
+++ 07-file-upload/starter/app.js
@@ -7,13 +7,24 @@ const app = express();
 // database
 const connectDB = require('./db/connect');
 
+// route
+const productRouter = require('./routes/productRoutes');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-
-app.get('/', (req, res) => {
-  res.send('<h1>File Upload Starter</h1>');
+const fileUpload = require('express-fileupload');
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
+
+app.use(express.static('./public'));
+app.use(express.json());
+app.use(fileUpload());
+app.use('/api/v1/products', productRouter);
 
 // middleware
 app.use(notFoundMiddleware);
---
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -0,0 +1,33 @@
+const Products = require('../models/Product');
+const path = require('path');
+const { StatusCodes } = require('http-status-codes');
+const customError = require('../errors');
+
+const uploadProductImage = async (req, res) => {
+  // check if file exist
+  if (!req.files) {
+    throw new customError.BadRequestError('No file uploaded');
+  }
+  const productImage = req.files.image;
+  // check file type
+
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new customError.BadRequestError('Please upload an image');
+  }
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new customError.BadRequestError(
+      `Please upload image smaller than ${maxSize} byte`
+    );
+  }
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + productImage.name
+  );
+  await productImage.mv(imagePath);
+  res
+    .status(StatusCodes.OK)
+    .send({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+module.exports = { uploadProductImage };
