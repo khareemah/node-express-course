b277edd3bbdf5c3bb20b08cdcdf6c302afde347a
kareemah <ajimobikareemah@gmail.com>
2023-03-16 17:50:39 +0700

    product controller structure

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -24,7 +24,7 @@ app.get('/api/v1', (req, res) => {
 });
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
-app.use('/api/v1/products');
+app.use('/api/v1/products', productRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
--- 10-e-commerce-api/starter/controller/productController.js
+++ 10-e-commerce-api/starter/controller/productController.js
@@ -1,19 +1,44 @@
 const Product = require('../models/Product');
+const { StatusCodes } = require('http-status-codes');
+const { NotFoundError } = require('../errors');
 const createProduct = async (req, res) => {
-  res.send('createProduct');
+  req.body.user = req.user.userId;
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.OK).json({ product });
 };
 const getAllProducts = async (req, res) => {
-  res.send('getAllProducts');
+  // add pagination later
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ count: products.length, products });
 };
 
 const getSingleProduct = async (req, res) => {
-  res.send('getSingleProduct');
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
+  if (!product) {
+    throw new NotFoundError(`No product with id ${productId}`);
+  }
+  res.status(StatusCodes.OK).json({ product });
 };
 const updateProduct = async (req, res) => {
-  res.send('updateProduct');
+  const { id: productId } = req.params;
+  const product = await Product.findOneAndUpdate({ _id: productId }, req.body, {
+    new: true,
+    runValidator: true,
+  });
+  if (!product) {
+    throw new NotFoundError(`No product with id ${productId}`);
+  }
+  res.status(StatusCodes.OK).json({ product });
 };
 const deleteProduct = async (req, res) => {
-  res.send('deleteProduct');
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
+  if (!product) {
+    throw new NotFoundError(`No product with id ${productId}`);
+  }
+  await product.remove();
+  res.status(StatusCodes.OK).json({ msg: 'Success! product removed' });
 };
 const uploadImage = async (req, res) => {
   res.send('uploadImage');
