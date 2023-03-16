5864fb885c3599eed4baa54285f1580b6e28b905
kareemah <ajimobikareemah@gmail.com>
2023-03-16 13:40:22 +0700

    product structure

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -11,6 +11,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
 
 app.use(morgan('tiny'));
 app.use(cors());
@@ -23,6 +24,7 @@ app.get('/api/v1', (req, res) => {
 });
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products');
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
---
+++ 10-e-commerce-api/starter/routes/productRoutes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
+const {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  uploadImage,
+} = require('../controller/productController');
+
+router
+  .route('/')
+  .get(getAllProducts)
+  .post(authenticateUser, authorizePermissions('admin'), createProduct);
+
+router
+  .route('/uploadImage')
+  .post(authenticateUser, authorizePermissions('admin'), uploadImage);
+router
+  .route('/:id')
+  .get(getSingleProduct)
+  .patch(authenticateUser, authorizePermissions('admin'), updateProduct)
+  .delete(authenticateUser, authorizePermissions('admin'), deleteProduct);
+module.exports = router;
