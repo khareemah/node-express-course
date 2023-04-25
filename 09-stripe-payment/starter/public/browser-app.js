8f773fde13965b83bf4de3b3ee0fc88b59489322
kareemah <ajimobikareemah@gmail.com>
2023-04-25 10:44:32 +0100

    stripe payment

--- 09-stripe-payment/starter/public/browser-app.js
+++ 09-stripe-payment/starter/public/browser-app.js
@@ -6,7 +6,7 @@ const total_amount = 10998;
 const shipping_fee = 1099;
 
 var stripe = Stripe(
-  'pk_test_51I87djFp5pnuKUXgBVIHiR36vVAWyfuyb7ckrhgyDNA1kM0GWHas9ZGUAgwJSFNUxrbyE6NwlMNmls1iGSfzHDdE00DQB3y6AH'
+  'pk_test_51N0hPfFj0uglbJc6bVntf2iyNNCiP8Qxxs5h5W4Z79uA0BwZT71N4ivya7JwVTmPoPBm0JXWlXl863lF729aCZdM008soKiksw'
 );
 
 // The items the customer wants to buy
